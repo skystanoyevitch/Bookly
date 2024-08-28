557bab0aeecdb377f7d9e30ca6b40fd78c0f8d75
Sky <skystanoyevitch@proton.me>
2024-08-28 09:50:26 -0700

    added stylesheet for title and text input

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet } from "react-native";
+import { useState } from "react";
+import { StyleSheet, TextInput, View } from "react-native";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -8,15 +9,50 @@ import { SafeAreaView } from "react-native-safe-area-context";
 // import { ThemedView } from "@/components/ThemedView";
 
 export default function HomeScreen() {
+  const [text, setText] = useState("");
   return (
-    <SafeAreaView>
-      <Text style={styles.textColor}>Marhaba</Text>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text
+          style={styles.title}
+          numberOfLines={2}
+          onPress={() => alert("Hello")}
+        >
+          This is an example of a Text component in React Native. Tap on me!
+        </Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(text) => setText(text)}
+          value={text}
+          placeholder="Enter Text Here"
+        >
+          <View></View>
+        </TextInput>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  textColor: {
+  title: {
     color: "blue",
+    textAlign: "center",
+    fontSize: 20,
+  },
+
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  textInput: {
+    borderColor: "blue",
+    borderWidth: 1,
+    margin: 15,
+    padding: 22,
+  },
+  textView: {
+    // backgroundColor: "green",
   },
 });
