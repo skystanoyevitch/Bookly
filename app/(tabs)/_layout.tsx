33c09b00d9a5c733fb372592fe07b1c85bfe5134
skysLaptop <sky.stanoyevitch@gmail.com>
2024-09-09 15:06:14 -0700

    deleted default components and references to those components

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,6 @@
 import { Tabs } from "expo-router";
 import React from "react";
 
-// import { TabBarIcon } from "@/components/navigation/TabBarIcon";
-// import { Colors } from "@/constants/Colors";
-// import { useColorScheme } from "@/hooks/useColorScheme";
-
 export default function TabLayout() {
   // const colorScheme = useColorScheme();
 
