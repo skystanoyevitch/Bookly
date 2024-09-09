431bb3138197ba7c764eb89227ae40a0db3c03b7
Sky <skystanoyevitch@proton.me>
2024-09-09 09:29:08 -0700

    removed old code and added placeholder text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -28,77 +28,7 @@ export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text
-          style={styles.title}
-          numberOfLines={2}
-          onPress={() => alert("Hello")}
-        >
-          This is an example of a Text component in React Native. Tap on me!
-        </Text>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={(text) => setText(text)}
-          value={text}
-          placeholder="Enter Text Here"
-        ></TextInput>
-        <Button title="Click Me!" color={"green"}></Button>
-        <View style={styles.buttonContainer}>
-          <Pressable style={styles.buttonStyle} onPress={buttonPressed}>
-            <Text>Click me too!</Text>
-          </Pressable>
-        </View>
-        <View>
-          <Image
-            source={require("../../assets/images/adaptive-icon.png")}
-            style={{ width: 200, height: 200 }}
-          />
-          <Image />
-          <ImageBackground
-            source={require("../../assets/images/bigsmiley.jpg")}
-            style={styles.imageBackgroundStyles}
-          >
-            <Text style={{ color: "white" }}>This is an Image Background</Text>
-          </ImageBackground>
-
-          <Text>Enable Feature:</Text>
-          <Switch
-            trackColor={{ false: "#767577", true: "#81b0ff" }}
-            thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-            onValueChange={toggleSwitch}
-            value={isEnabled}
-          />
-
-          <StatusBar
-            barStyle="light-content"
-            backgroundColor="#6A0E37"
-            hidden={false}
-            animated={true}
-            translucent={true}
-          />
-          <Modal
-            animationType="slide"
-            transparent={false}
-            visible={modalVisible}
-          >
-            <SafeAreaView>
-              <View>
-                <Text>Wello Horld!</Text>
-                <TouchableHighlight
-                  onPress={() => setmodalVisible(!modalVisible)}
-                >
-                  <Text>Hide Modal</Text>
-                </TouchableHighlight>
-              </View>
-            </SafeAreaView>
-          </Modal>
-          <TouchableHighlight
-            onPress={() => {
-              setmodalVisible(true);
-            }}
-          >
-            <Text>Show Modal</Text>
-          </TouchableHighlight>
-        </View>
+        <Text>Home Page</Text>
       </View>
     </SafeAreaView>
   );
