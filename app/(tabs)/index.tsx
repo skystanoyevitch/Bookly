f7bfa2a300a5404ded5989e5ab05dada891be9e5
Sky <skystanoyevitch@proton.me>
2024-09-11 10:05:19 -0700

    create logic for barcode scanner and permissions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,51 @@
-import { useState } from "react";
+import { CameraView, Camera } from "expo-camera";
+import { useEffect, useState } from "react";
 import { Button, Pressable, StyleSheet, View } from "react-native";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  // const [text, setText] = useState("");
-  // const [isEnabled, setisEnabled] = useState(false);
-  // const [modalVisible, setmodalVisible] = useState(false);
+  const [hasPermission, setHasPermission] = useState<any | null>(null);
+  const [scanner, setScanner] = useState(false);
 
-  // const toggleSwitch = () => setisEnabled((previousState) => !previousState);
-  const buttonPressed = () => {
-    alert("button pressed!");
+  useEffect(() => {
+    const getCameraPermissions = async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getCameraPermissions();
+  }, []);
+
+  const handleBarcodeScanner = ({ type, data }: any) => {
+    setScanner(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for Camera Permissions</Text>;
+  }
+
+  if (hasPermission === false) {
+    return <Text> No Access to Camera</Text>;
+  }
+
+  // const buttonPressed = () => {
+  //   alert("button pressed!");
+  // };
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Pressable onPress={buttonPressed}>
-          <Text style={styles.buttonStyle}>Add Book</Text>
+        {/* <CameraView
+            onBarcodeScanned={scanner ? undefined : handleBarcodeScanner}
+            barcodeScannerSettings={{
+              barcodeTypes: [],
+            }}
+          />
+          {scanner && (
+          )} */}
+        <Pressable>
+          <Text onPress={() => setScanner(false)}>Tap to scan again</Text>
         </Pressable>
       </View>
     </SafeAreaView>
