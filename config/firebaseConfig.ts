54159b359615a5d9d970b5e59aa0992750df4a4e
Sky <skystanoyevitch@proton.me>
2024-09-26 15:14:53 -0700

    installed dotenv package + updated API keys

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,12 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
+import "dotenv/config";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDOw0bOAjigez1tmLkaLkTQRp-TacyPbq4",
+  apiKey: process.env.FIREBASE_API,
   authDomain: "bookly-react-native-app.firebaseapp.com",
   projectId: "bookly-react-native-app",
   storageBucket: "bookly-react-native-app.appspot.com",
