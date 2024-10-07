5e6ec4581fd1b8a6aaa03b3aab90d57a6e9701e6
Sky <skystanoyevitch@proton.me>
2024-10-07 11:37:08 -0700

    removed import dotenv

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import "dotenv/config";
+// require("dotenv").config();
+// console.log(process.env);
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
