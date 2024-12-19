c55f5be6f4f207a6feb05a7a05eb0a6cad52ac0c
Sky Stanoyevitch <skystanoyevitch@proton.me>
2024-12-19 10:17:27 -0800

    desktop: fixed proper data not passing through to localparams from router

--- app/(AddBook)/BookOptions.tsx
+++ app/(AddBook)/BookOptions.tsx
@@ -1,12 +1,53 @@
-import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { FIRESTORE_DB } from "@/config/firebaseConfig";
+import { router, useLocalSearchParams, useRouter } from "expo-router";
+import { addDoc, collection } from "firebase/firestore";
+import { Text, TouchableOpacity, View } from "react-native";
 
 const BookOptions = () => {
-  const { description } = useLocalSearchParams();
-  console.log("add book page", `${description}`);
+  // const router = useRouter();
+  const { bookId, title, authors, description, volumeInfo }: any =
+    useLocalSearchParams();
+  const parsedPreferences = volumeInfo ? JSON.parse(volumeInfo) : null;
+
+  // const { id, [searchInfo], textSnippet }: any = useLocalSearchParams();
+  // console.log("Destructured data", id, searchInfo, textSnippet);
+  console.log(
+    "params:",
+    bookId,
+    title,
+    authors,
+    description,
+    "info:",
+    parsedPreferences.info.title
+  );
+
+  // const addBook = async () => {
+  //   // After user successfully scanned book, add to newBook object
+  //   const newBook = {
+  //     // bookId: id,
+  //     title: "",
+  //   };
+
+  //   try {
+  //     const db = await addDoc(collection(FIRESTORE_DB, "users"), newBook);
+  //     router.push("/");
+  //   } catch (error) {
+  //     console.log("error adding document", error);
+  //   }
+  // };
+
   return (
     <View>
-      <Text>Description: {description}</Text>
+      <Text>Title: {parsedPreferences?.info.title}</Text>
+      <Text>Author: {parsedPreferences?.info.authors}</Text>
+      <Text>Description: {parsedPreferences?.info.description}</Text>
+
+      {/* <TouchableOpacity onPress={addBook}>
+        <Text>Add Book</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text>Cancel</Text>
+      </TouchableOpacity> */}
     </View>
   );
 };
