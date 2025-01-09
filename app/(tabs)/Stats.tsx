9527c90139065ec6d57ae7a8a7d16ee9b5e1ec27
Sky Stanoyevitch <skystanoyevitch@proton.me>
2025-01-09 15:06:57 -0800

    refactor(Stats): rename component to BookshelfScreen and add styling for layout

--- app/(tabs)/Stats.tsx
+++ app/(tabs)/Stats.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-const Stats = () => {
+const BookshelfScreen: React.FC = () => {
   return (
-    <View>
-      <Text>stats</Text>
+    <View style={styles.container}>
+      <Text>Stats Screen</Text>
     </View>
   );
 };
 
-export default Stats;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default BookshelfScreen;
